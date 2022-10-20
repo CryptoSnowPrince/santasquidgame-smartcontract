15e1026a8a8e4c38d57b70e7a0033fcc11bad951
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-10-20 17:37:53 +0900

    remove addLiquidity

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -730,21 +730,6 @@ contract SNOWPRINCE is Context, IERC20, Ownable {
         
         emit SwapETHForTokens(amount, path);
     }
-    
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-        // approve token transfer to cover all possible scenarios
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
-
-        // add the liquidity
-        uniswapV2Router.addLiquidityETH{value: ethAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            owner(),
-            block.timestamp
-        );
-    }
 
     function _tokenTransfer(address sender, address recipient, uint256 amount,bool takeFee) private {
         if(!takeFee)
